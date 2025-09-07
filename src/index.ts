f638085b967580d245d6bba2c3af779b193f97c9
NureddinFarzaliyev <nureddin.fa@gmail.com>
2025-09-07 16:37:02 +0400

    init: swagger documentation

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,23 @@
 import express from "express";
 import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocsV1 from "@/shared/tools/swagger";
 
+// App
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Middlewares
 app.use(express.json());
 app.use(cors());
+app.use("/v1/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocsV1));
 
+// Routes
 app.get("/", (_, res) => {
   res.send("Hello, World!");
 });
 
+// Initialize
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
---
+++ src/shared/tools/swagger.ts
@@ -0,0 +1,21 @@
+import swaggerJSDoc from "swagger-jsdoc";
+
+const swaggerOptionsV1 = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "E-Commerce Web API",
+      version: "1.0.0",
+      description: "API documentation for E-Commerce Web",
+    },
+    servers: [
+      {
+        url: ["http://localhost:3000", process.env.BASE_URL],
+      },
+    ],
+  },
+  apis: ["./docs/**/*.yaml", "./src/routes/*.ts"],
+};
+
+const swaggerDocsV1 = swaggerJSDoc(swaggerOptionsV1);
+export default swaggerDocsV1;
