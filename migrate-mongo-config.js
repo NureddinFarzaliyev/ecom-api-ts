b1e004b9efc1653905f39caffaeeb86674cc7fbe
NureddinFarzaliyev <nureddin.fa@gmail.com>
2025-12-26 11:25:57 +0400

    init: migrate-mongo

---
+++ migrate-mongo-config.js
@@ -0,0 +1,19 @@
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+
+const config = {
+  mongodb: {
+    url: `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.bfs8zjk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
+    databaseName: "test",
+    options: {},
+  },
+  migrationsDir: "migrations",
+  changelogCollectionName: "changelog",
+  lockCollectionName: "changelog_lock",
+  lockTtl: 0,
+  migrationFileExtension: ".js",
+  useFileHash: false,
+  moduleSystem: "commonjs",
+};
+
+module.exports = config;
